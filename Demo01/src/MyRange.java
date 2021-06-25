dc4c1a1c2a07066e5cfac4120deac2ed6d7a03ba
mookkiko <tanyaluk.id@gmail.com>
2021-06-25 20:40:21 +0700

    fix MyRange final

--- Demo01/src/MyRange.java
+++ Demo01/src/MyRange.java
@@ -14,19 +14,28 @@
     *5) => 5-1 =5
  4. result = 1,2,3,4,5**/
 
+
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyRange {
 
     private final String input;
 
+
     public MyRange(String input) {
         this.input = input;
     }
 
+
     public boolean checkStartWithInclude() {
         return input.startsWith("[");
+    }
+
+    public boolean checkStartWithInclude2() {
+        return input.endsWith("]");
     }
 
     public int getFirstNumber() {
@@ -39,30 +48,29 @@ public class MyRange {
 
     }
 
-    public String getResult() {
-        int firstNumber = getFirstNumber();
-        int secondNumber = 5;
-//        String[] results = new String[secondNumber - firstNumber + 1];
-        // Collection Framework
-        List<String> results = new ArrayList<>();
-        for (int i = firstNumber; i < secondNumber + 1 ; i++) {
-            results.add(String.valueOf(i));
-        }
-        return String.join(",", results);
-    }
-
-
-    public boolean checkStartWithExclude() {
-            return input.startsWith("(");
-    }
-
     public int getLastNumber() {
-        int LastNumber = Character.getNumericValue(input.charAt(1));
-        if( checkStartWithExclude() ) {
+        int LastNumber = 5;
+        if( checkStartWithInclude2() ) {
             return LastNumber;
         }
         return LastNumber - 1;
 //        return input.charAt(1) - 48;
 
     }
+
+    public String getResult() {
+        int firstNumber = getFirstNumber();
+        int LastNumber = getLastNumber();
+        //int secondNumber = 5;
+//        String[] results = new String[secondNumber - firstNumber + 1];
+        // Collection Framework
+        List<String> results = new ArrayList<>();
+        for (int i = firstNumber; i < LastNumber + 1 ; i++) {
+            results.add(String.valueOf(i));
+        }
+        return String.join(",", results);
+    }
+
+
+
 }
--- Demo01/src/MyRangeTest.java
+++ Demo01/src/MyRangeTest.java
@@ -10,17 +10,30 @@ class MyRangeTest {
         boolean isInclude = myRange.checkStartWithInclude();
         assertTrue(isInclude);
     }
+    @Test
+    public void startWithInclude2() {
+        MyRange myRange = new MyRange("[1,5]");
+        boolean isInclude = myRange.checkStartWithInclude2();
+        assertTrue(isInclude);
+    }
 
     @Test
-    public void startWithExclude() { // [
+    public void startWithExclude() {
         MyRange myRange = new MyRange("(1,5]");
-        boolean isInclude = myRange.checkStartWithExclude();
+        boolean isInclude = myRange.checkStartWithInclude();
+        assertFalse(isInclude);
+    }
+
+    @Test
+    public void startWithExclude2() {
+        MyRange myRange = new MyRange("[1,5)");
+        boolean isInclude = myRange.checkStartWithInclude2();
         assertFalse(isInclude);
     }
 
     @Test
     public void getFirstNumberAndStartWithInclude() { // [
-        MyRange myRange = new MyRange("[1,5)");
+        MyRange myRange = new MyRange("[1,5]");
         int firstNumber = myRange.getFirstNumber();
         assertEquals(1, firstNumber);
     }
@@ -30,6 +43,20 @@ class MyRangeTest {
         MyRange myRange = new MyRange("(1,5]");
         int firstNumber = myRange.getFirstNumber();
         assertEquals(2, firstNumber);
+    }
+
+    @Test
+    public void getLastNumberAndStartWithInclude() { // [
+        MyRange myRange = new MyRange("[1,5]");
+        int LastNumber = myRange.getLastNumber();
+        assertEquals(5, LastNumber);
+    }
+
+    @Test
+    public void getLastNumberAndStartWithExclude() { // [
+        MyRange myRange = new MyRange("[1,5)");
+        int LastNumber = myRange.getLastNumber();
+        assertEquals(4, LastNumber);
     }
 
     @Test
@@ -43,21 +70,21 @@ class MyRangeTest {
     public void result02() { // [
         MyRange myRange = new MyRange("(1,5]");
         String result = myRange.getResult();
-        assertEquals("1,2,3,4,5", result);
+        assertEquals("2,3,4,5", result);
     }
 
     @Test
     public void result03() { // [
         MyRange myRange = new MyRange("[1,5)");
         String result = myRange.getResult();
-        assertEquals("1,2,3,4,5", result);
+        assertEquals("1,2,3,4", result);
     }
 
     @Test
     public void result04() { // [
         MyRange myRange = new MyRange("(1,5)");
         String result = myRange.getResult();
-        assertEquals("2,3,4,5", result);
+        assertEquals("2,3,4", result);
     }
 
 }
