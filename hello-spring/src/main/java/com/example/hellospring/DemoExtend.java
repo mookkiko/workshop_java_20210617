ee8ef1d5fbea2e1d9da8e326332b80c5debf0df3
mookkiko <tanyaluk.id@gmail.com>
2021-06-25 23:39:13 +0700

    abstract class

---
+++ hello-spring/src/main/java/com/example/hellospring/DemoExtend.java
@@ -0,0 +1,57 @@
+package com.example.hellospring;
+
+public class DemoExtend {
+    public static void main(String[] args) {
+        Report1 report1 = new Report1();
+        report1.build();
+    }
+}
+
+interface Process {
+    void generateHeader();
+    void generateBody();
+    void generateFooter();
+}
+
+class Report2 implements  Process {
+    @Override
+    public void generateHeader() {
+    }
+
+    @Override
+    public void generateBody() {
+    }
+
+    @Override
+    public void generateFooter() {
+    }
+}
+
+abstract class Report {
+    abstract void generateHeader();
+    abstract void generateBody();
+    abstract void generateFooter();
+    public void build() {
+        generateHeader();
+        generateBody();
+        generateFooter();
+    }
+}
+
+class Report1 extends Report {
+    @Override
+    void generateBody() {
+        System.out.println("Generate Body");
+    }
+
+    @Override
+    void generateHeader() {
+        System.out.println("Generate Header");
+    }
+
+    @Override
+    void generateFooter() {
+        System.out.println("Generate Footer");
+    }
+}
+
