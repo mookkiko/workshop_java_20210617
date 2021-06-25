550e9eee23d392215592a14a4d215f3feb452606
mookkiko <tanyaluk.id@gmail.com>
2021-06-25 22:11:46 +0700

    start workshop2 Sorting

--- Demo01/src/DemoCollectionFramework.java
+++ Demo01/src/DemoCollectionFramework.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class DemoCollectionFramework {
     public static void main(String[] args){
+        //Map
         Map<Integer , Employee> employeeMap = new HashMap<>();
         employeeMap.put(1, new Employee("f1","l1"));
         employeeMap.put(2, new Employee("f2","l2"));
---
+++ Demo01/src/Mysort.java
@@ -0,0 +1,25 @@
+/*Class=MySort
+Input
+ * List of Integer = List<Integer>
+Output
+ * List of sorted number
+Input        Expected
+[5,4,3,2,1]  [1,2,3,4,5]
+
+
+
+Test cases
+[] => []
+[1] => [1]
+[1,2] => [1,2]
+[2,1] => [1,2]
+[1,2,3] => [1,2,3]
+[2,1,3] => [1,2,3]
+[1,3,2] => [1,2,3]
+[3,2,1] => [1,2,3]
+[5,4,3,2,1]  [1,2,3,4,5]*/
+
+public class Mysort {
+
+
+}
